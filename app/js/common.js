0b2578bc5d6fa5f80fb5da3f2d4ba6414f1b80ac
viktoriazaharova <zaharova181114@gmail.com>
2020-06-19 19:35:03 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+$('.advantages-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+});
