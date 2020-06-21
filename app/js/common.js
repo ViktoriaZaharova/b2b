e008e5078874483007247beac55a6ea576db9da0
viktoriazaharova <zaharova181114@gmail.com>
2020-06-21 12:28:21 +0300

    add block project

--- app/js/common.js
+++ app/js/common.js
@@ -4,3 +4,18 @@ $('.advantages-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>'
 });
+
+$('.project-slider__photo').slick({
+    slidesToShow: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    asNavFor: '.project-slider__content'
+});
+
+$('.project-slider__content').slick({
+    slidesToShow: 1,
+    dots: false,
+    arrows: false,
+    asNavFor: '.project-slider__photo'
+});
